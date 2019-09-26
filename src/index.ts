320a03124a985aa9f6de32d75765705efdc7a5ca
luigitercero <luigitercero3@gmail.com>
2019-09-25 20:17:08 -0600

    introduccion

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,39 @@
+//tipado typescript
 
-import Server from './server/server';
-  //import router from './router/router';
-   
-const server = Server.init(3000);
-  
-//server.app.use(router);
-server.start(()=>console.log("server starter"));
+let bol:boolean = true;
+let num:number =1.555;
+let str:string = "hola";
+let cad:number[] = [1,5];
+let cualquer:any[] = [bol,num,str,cad];
+
+//objetos
+
+let obj: {nombre:string, edad:number, abilidad:string[]} ={
+  nombre:"luis",
+  edad:24,
+  abilidad:["hablar en publico"]
+}
+
+//tipos personalizados
+//buena practica colocarlo con mayuscula
+
+type Heroe = {nombre:string, edad:number, abilidad:string[]}
+
+let lui: Heroe ={
+  nombre:"luis",
+  edad:24,
+  abilidad:["hablar en publico"]
+}
+
+//crear Clases
+
+class Alumno {
+  nombre:string;
+
+  constructor(nombre:string){
+    this.nombre = nombre;
+  }
+
+}
+
+let al:Alumno = new Alumno("luis");
